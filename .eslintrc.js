66576fde3417ef07f776610b75fb6611d30f618b
Fahad Hossain <oneloopone@gmail.com>
2016-05-24 03:53:39 +0600

    use env variables as fallback

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": 0,
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0
+    }
+};
--- bin/cli.js
+++ bin/cli.js
@@ -1,103 +1,116 @@
 #!/usr/bin/env node
-const request = require('request');
 const argv = require('minimist')(process.argv.slice(2));
 const spawn = require('child_process').spawn;
 const inquirer = require('inquirer');
+const axios = require('axios');
 
 
 function git_url(org_name) {
   return "https://api.github.com/orgs/" + org_name + "/repos";
-};
+}
 
 
-function get_repos(username, password, org_name, cb) {
-  let params = {
-    url: git_url(org_name),
+function get_clone_repos(info) {
+  let http_params = {
+    url: git_url(info.org),
     auth: {
-      user: username,
-      pass: password
+      username: info.username,
+      password: info.password
     },
-    json: true,
     headers: {
       'User-Agent': 'Git Clone Org'
     }
   };
-  request(params, function(err, res, body) {
-    if (body !== undefined) {
-      if(body['message']){
-        console.error(body.message);
-      }else{
-        let i, len, obj;
-        let urls = [];
-        for (i = 0, len = body.length; i < len; i++) {
-          obj = body[i];
-          urls.push(obj['clone_url']);
+
+  let fetcher = axios.create();
+
+  fetcher
+    .request(http_params)
+    .then(function(response) {
+      if(response['data']){
+        if (response.data.message){
+          console.error(response.data.message);
+        }else{
+          let links = [];
+          response.data.forEach(repo => links.push(repo['clone_url']));
+          confirm_clone(links);
         }
-        inquirer.prompt([{
-            name: 'confirm',
-            message: 'Got ' + urls.length + ' repos. Confirm clone',
-            default: true,
-            type: 'confirm'
-          }]).then(function(res) {
-            if(res.confirm){
-              cb(urls);
-            }
-          });
       }
-    }
-  });
-};
+    });
+}
 
 
-function git_clone_org(org, callback, callback_final) {
+function confirm_clone(links) {
+  let confirm = true;
+  inquirer
+    .prompt({
+      name: 'confirm',
+      message: 'Got ' + links.length + ' repo(s). Confirm clone',
+      default: true,
+      type: 'confirm'
+    })
+    .then(function(res) {
+      if(res.confirm)
+        links.forEach(link => clone(link));
+      else
+        console.log('Clone cancelled!')
+    });
+  return confirm;
+}
+
+
+function git_clone_org(org) {
   let options = [
     {
       name: 'username',
       message: 'Username for github.com',
-      type: 'input'
+      type: 'input',
+      default: process.env.GITHUB_USER || ''
     }, {
       name: 'password',
       message: 'Password (will not be echoed)',
-      type: 'password'
+      type: 'password',
+      default: process.env.GITHUB_PASSWORD || '****'
     }
   ];
+
   if (!org){
     options.push({
       name: 'org',
       message: 'Name of GitHub organization',
       type: 'input'
-    })
+    });
   }
-  inquirer.prompt(options).then(function(res) {
-    if(res){
-      let auth = {};
-      auth.username = res.username;
-      auth.password = res.password;
-      auth.org = org || res.org;
-      callback(auth.username, auth.password, auth.org, callback_final);
-    }
-  });
-};
-
-function clone(repos){
-  let i, len;
-  for (i = 0, len = repos.length; i < len; i++) {
-    url = repos[i];
-    let cloner = spawn('git', ['clone', url]);
-    cloner.stdout.on('data', function(data) {
-      console.log(data.toString());
-    });
-    cloner.stderr.on('data', function(data) {
-      console.error(data.toString());
-    });
-    cloner.on('exit', function(code) {
-      if (code !== 0) {
-        console.error("Exited with code " + code);
+  inquirer
+    .prompt(options)
+    .then(function(res) {
+      if(res){
+        let auth = res;
+        auth.org = org || res.org;
+        get_clone_repos(auth);
       }
     });
-  }
-};
+}
+
+function clone(url){
+  let cloner = spawn('git', ['clone', url]);
+  
+  cloner.stdout.on('data', function(data) {
+    console.log(data.toString());
+  });
+  
+  cloner.stderr.on('data', function(data) {
+    console.error(data.toString());
+  });
+  
+  cloner.on('exit', function(code) {
+    if (code !== 0)
+      console.error("Exited with code " + code);
+    else
+      console.log("Cloned repo successfully");
+  });
+}
 
 if (require.main === module) {
-  git_clone_org(argv['_'][0], get_repos, clone);
+  git_clone_org(argv['_'][0]);
 }
