48db23fcabd9dd85b6102794867f8c0beb160598
Fahad Hossain <oneloopone@gmail.com>
2016-05-27 07:22:13 +0600

    clone all repos using pagination, fallback to env variables acknowledge virtualenv

--- README.md
+++ README.md
@@ -6,9 +6,14 @@ Clone all repos from a github organization
 ```bash
 git clone-org myawesometeam
 ```
-where **myawesometeam** is the github organization.
-
-_Note: This script is very rudimentary, make a PR if you wanna add some cool feature._
+Where **myawesometeam** is the github organization.
+_Protip: if you have GITHUB_USER and GITHUB_PASSWORD set for [hub](http://hub.github.com), you can just skip the username and password input (Press enter twice)_
 
 ## Installation:
-On \*nix systems you can just run `bash install.sh`. No idea about windows :3
+* Clone this repo into a new virtualenv (or you can go global)
+* Install the requirements:
+    `pip install -r requirements.txt`
+* Run `bash install.sh`
+No idea about Windows :3
+
+_Note: This script is very rudimentary, make a PR if you wanna add some cool feature._
--- main.py
+++ main.py
@@ -3,29 +3,42 @@
 Clones all the repos in a github organization.
 """
 
+from subprocess import call as sys_call
+from getpass import getpass
 import requests
 import argparse
-import subprocess
 import sys
-import getpass
+import re
+import os
 
 
 GITHUB_URL = "https://api.github.com/orgs/{0}/repos"
 
 
-def get_repos(url=None, username=None, password=None):
+def get_repos(url=None, username=None, password=None, page=None):
     """
     Get a list of all repos from the supplied URL
     """
     if url and username and password:
-        response = requests.get(url, auth=(username, password))
+        params = {
+            'per_page': 100,
+            'page': page if page else 1
+        }
+        response = requests.get(url, auth=(username, password), params=params)
         json_decoded = response.json()
         try:
             repos = [item['clone_url'] for item in json_decoded]
-            return repos
         except:
             print("Possibly wrong password")
             return False
+        next_page = None
+        header_link = response.headers.get('link');
+        if header_link:
+            for link in header_link.split(','):
+                if re.search('next', link):
+                    rx_match = re.search('(?<!per_)page=(\d+)', link)
+                    next_page = rx_match.groups()[0]
+        return (repos, next_page)
     else:
         return False
 
@@ -35,13 +48,16 @@ def clone_repos(repos_list=None):
     Clones all the repos in the provided list
     """
     if repos_list:
-        returns = [subprocess.call(["git", "clone", repo]) for repo in repos_list]
-        if any(returns):
-            return True
+        num_repos = len(repos_list)
+        confirm = input("Found {0} repo(s). Clone? [y/n] ".format(num_repos))
+        if re.search('(yes|Yes|YES|Y|y)', confirm):
+            returns = [sys_call(["git", "clone", repo]) for repo in repos_list]
+            return sum(returns)
         else:
-            return False
+            print("Cloning cancelled.")
+            return 0
     else:
-        return True
+        return 1
 
 
 def cli():
@@ -50,18 +66,34 @@ def cli():
     arguments = parser.parse_args()
 
     if arguments.org_name:
+        org_name = arguments.org_name[0]
         user = input("Username for github.com: ")
-        password = getpass.getpass("Password for {0} (won't be echoed):".format(user))
-        repos = get_repos(GITHUB_URL.format(arguments.org_name[0]), user, password)
-
-        if not repos:
-            return 255
-        if clone_repos(repos):
-            return 255
+        if user == '':
+            user = os.getenv('GITHUB_USER')
+        password = getpass("Password for {0} (won't be echoed): ".format(user))
+        if password == '':
+            password = os.getenv('GITHUB_PASSWORD')
+        ret_codes = 0
+        next_page = 1
+        while True:
+            response = get_repos(GITHUB_URL.format(org_name), user,
+                password, page=next_page)
+            if response:
+                (repos, next_page) = response
+            else:
+                ret_codes += 255
+                break
+            if repos:
+                ret_codes += clone_repos(repos)
+            else:
+                print('No repos returned. Something probably went wrong.')
+                break
+            if not next_page:
+                break
+                
+        return ret_codes
     else:
         return 255
-
-    return 0
 
 
 if __name__ == "__main__":
